2be78d84190903905c04951ceefd9ded981efd3e
Miklos Maroti <mmaroti@gmail.com>
2017-05-15 13:18:27 -0500

    better login handling

--- html/js/c2stem.js
+++ html/js/c2stem.js
@@ -3,8 +3,7 @@ function C2stem(snapCloudUrl) {
     this.withCredentials = false;
 };
 
-C2stem.prototype.login = function (username, password, callback) {
-    console.log("login", username, password);
+C2stem.prototype.login = function (username, password, remember, callback) {
     try {
         var req = new XMLHttpRequest();
         req.open('POST', this.snapCloudUrl + "/SnapCloud/", true);
@@ -13,19 +12,39 @@ C2stem.prototype.login = function (username, password, callback) {
         req.onreadystatechange = function () {
             if (req.readyState === 4) {
                 if (req.status === 200) {
-                    var session = req.getResponseHeader('MioCracker')
-                        .split(';')[0];
-                    callback(null, session);
+                    callback(null);
                 } else {
                     callback(req.responseText || 'Connection refused');
                 }
             }
         };
         req.send(JSON.stringify({
-            '__h': hex_sha512(password),
-            '__u': username
+            '__h': password && hex_sha512(password),
+            '__u': username,
+            'remember': !!remember,
         }));
     } catch (err) {
         callback(err || "Could not login");
+    }
+};
+
+C2stem.prototype.logout = function (callback) {
+    console.log("logout");
+    try {
+        var req = new XMLHttpRequest();
+        req.open('GET', this.snapCloudUrl + "/SnapCloud/logout", true);
+        req.withCredentials = this.withCredentials;
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
+                if (req.status === 200) {
+                    callback(null);
+                } else {
+                    callback(req.responseText || 'Connection refused');
+                }
+            }
+        };
+        req.send();
+    } catch (err) {
+        callback(err || "Could not logout");
     }
 };
