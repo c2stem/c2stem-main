4fce4a30dac584e1d27158daa6a38c81b8345e15
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2017-05-17 12:40:20 -0500

    - changed to call to load components when snap is loaded.

--- html/components/component_root.js
+++ html/components/component_root.js
@@ -1,10 +1,11 @@
 /**
  * Created by hasanm on 5/9/2017.
  */
-function load_modules() {
+function load_components() {
     // load data of all the modules
     conceptual_model_load_data();
     execute_when_ready(load_views, 100);
+    console.log("load_components");
 }
 
 function load_views() {
@@ -14,7 +15,7 @@ function load_views() {
 
 function execute_when_ready(action, wait_period) {
     console.log("execute_when_ready");
-    if (!is_ready_modules()) {
+    if (!is_ready_components()) {
         setTimeout(function () {
             execute_when_ready(load_views, wait_period);
         }, wait_period);
@@ -24,12 +25,12 @@ function execute_when_ready(action, wait_period) {
 }
 
 
-$(document).ready(function () {
-    load_modules();
-});
+// $(document).ready(function () {
+//     load_components();
+// });
 
 
-function is_ready_modules() {
+function is_ready_components() {
     if (!is_ready_cm())
         return false;
     return true;
