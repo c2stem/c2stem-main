524984210b2527ea1105bc63d717c729d824c5c9
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2017-06-20 17:18:23 -0500

    created some ActionManagers to log events of various parts

---
+++ html/js/action managers/TaskListActionManager.js
@@ -0,0 +1,32 @@
+/**
+ * Created by hasanm on 6/16/2017.
+ */
+var TaskViewActionManger = new ActionManager();
+
+TaskViewActionManger.addActions(
+    'switchTab',
+    'pageLoaded'
+);
+
+TaskViewActionManger._switchTab = function(tab_name, tab_type) {
+    return [tab_name, tab_type];
+};
+
+TaskViewActionManger.onSwitchTab = function(tab_name, tab_type) {
+};
+
+TaskViewActionManger._pageLoaded = function(task_id, module_id) {
+    return [task_id, module_id];
+};
+
+TaskViewActionManger.onPageLoaded = function(task_id, module_id) {
+};
+
+///////////////// using the action manager /////////////////
+TaskViewActionManger.assignId('task-view');  // this will also register the action manager
+
+
+// USAGE - To log an event
+// $('#delete-btn').click(function() {
+//     SampleActionManager.sampleEvent(concept);
+// });
