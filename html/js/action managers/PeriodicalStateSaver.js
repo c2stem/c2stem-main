73a94c981475f5999e062503fc02f44bb5329f36
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2017-06-21 18:24:04 -0500

    Implementing logic for AutoSaving user progress

--- html/js/action managers/PeriodicalStateSaver.js
+++ html/js/action managers/PeriodicalStateSaver.js
@@ -4,32 +4,44 @@
 var PeriodicalStateSaver = new ActionManager();
 
 PeriodicalStateSaver.addActions(
-    'saveModelState'
+    'logModelState'
 );
 
-PeriodicalStateSaver._saveModelState = function() {
+PeriodicalStateSaver._logModelState = function() {
     var state = {};
     console.log("Collecting the state of the models");
     var userTaskData = c2stem.collectUserProgressData(false);
     return [userTaskData];
 };
 
-PeriodicalStateSaver.onSaveModelState = function(state) {
+PeriodicalStateSaver.onLogModelState = function(state) {
 };
 
 PeriodicalStateSaver.assignId('periodical-state-saver');
 
+var timeLastLogged = new Date().getTime();
 var timeLastSaved = new Date().getTime();
 C2StemActions.addEventListener(function (event) {
-    var curTime = new Date().getTime();
-    var timeElasped = curTime - timeLastSaved;
-    console.log("timeElasped:",timeElasped);
-    // if time elapsed not equal to 1 minute
-    if(timeElasped < 10 * 1000)
+    if(event.namespace === 'periodical-state-saver')
         return;
-    timeLastSaved = curTime;
+    // loging the model state
+    var curTime = new Date().getTime();
+    var timeElasped = curTime - timeLastLogged;
+    console.log("log timeElasped:",timeElasped);
+    // if time elapsed not equal to 1 minute
+    if(timeElasped > 10 * 1000){
+        timeLastLogged = curTime;
 
-    // console.log("A Logging Event occured:", event);
-    if(event.namespace !== 'periodical-state-saver')
-        PeriodicalStateSaver.saveModelState(event);
+            PeriodicalStateSaver.logModelState(event);
+    }
+
+    // auto saving the model state
+    curTime = new Date().getTime();
+    timeElasped = curTime - timeLastSaved;
+    console.log("autosave timeElasped:",timeElasped);
+    // if time elapsed not equal to 1 minute
+    if(timeElasped > 63 * 1000){
+        timeLastSaved = curTime;
+        c2stem.saveUserProgress();
+    }
 });
