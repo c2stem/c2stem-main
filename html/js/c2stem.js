1700e79fca5999a1ee5ea65a3e0483861dfa6263
Brian Broll <brian.broll@vanderbilt.edu>
2017-06-22 16:25:56 -0500

    Return the original snap actions object for compatibility

--- html/js/c2stem.js
+++ html/js/c2stem.js
@@ -162,8 +162,7 @@ C2Stem.prototype.addSnap1Tab = function (id, name, template, callbackOnLoad) {
          snapWindow.SnapActions.applyEvent = function (event) {
              if (!event.namespace) { // route all internal events through C2StemActions
                  event.namespace = snapId;
-                 return C2StemActions.applyEvent(event);
-             } else {
+                 C2StemActions.applyEvent(event);
                  return snapWindow.ActionManager.prototype.applyEvent.call(this, event);
              }
          };
