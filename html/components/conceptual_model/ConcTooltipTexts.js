0a23a8837ba1df9f9ce4f995fca8ca6b934524dd
NicoleHutchins <nicole.hutchins@gmail.com>
2017-09-03 17:46:58 -0500

    Edits to module one (re: graphing task references) Conceptual model tool tips edits.

--- html/components/conceptual_model/ConcTooltipTexts.js
+++ html/components/conceptual_model/ConcTooltipTexts.js
@@ -3,5 +3,19 @@
  */
 var TooltipTexts = {
     // "elementID of a concept" : "tooltip text",
-    "PositionX" : "This is a custom tooltip for PositionX"
-};
+    "physicsXPosition": "A variable that stores the value of the object's x position. You will also be given blocks to set and change the variable.",
+    "physicsYPosition": "A variable that stores the value of the object's y position. You will also be given blocks to set and change the variable.",
+    "xVelocity": "A variable that stores the value of the object's x velocity. You will also be given blocks to set and change the variable.",
+    "yVelocity": "A variable that stores the value of the object's y velocity. You will also be given blocks to set and change the variable.",
+    "xAcceleration": "A variable that stores the value of the object's x acceleration. You will also be given blocks to set the variable.",
+    "yAcceleration": "A variable that stores the value of the object's y acceleration. You will also be given blocks to set the variable.",
+    "physicsAngle": "A variable that stores the value of the object's heading. You will also be given blocks to set and change the variable.",
+    "update_position": "This behavior block should be customized to program how the object will change its position given its velocity/velocities.",
+    "update_velocity": "This behavior block should be customized to program how the object will change its velocity given its acceleration(s).",
+    "speed_up": "This behavior block should be customized to program how and under what conditions the object will speed up.",
+    "slow_down": "This behavior block should be customized to program how and under what conditions the object will slow down.",
+    "come_to_stop": "This behavior block should be customized to program how and under what conditions the object will come to a stop.",
+    "maintain_speed": "This behavior block should be customized to program how and under what conditions the object will maintain its speed.",
+    "update_resultant_velocity": "This behavior block should be customized to program how and under what conditions the object will update its x and y velocities.",
+    "fly_backward" : "This behavior block should be customized to program how and under what conditions the object will fly backward."
+}
